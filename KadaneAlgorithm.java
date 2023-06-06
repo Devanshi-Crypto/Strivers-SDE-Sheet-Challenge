84943e3c89f852b40863bf5c662e2a1177af3cf4
Devanshi Doshi <76578430+Devanshi-Crypto@users.noreply.github.com>
2023-06-06 09:34:20 +0530

    Kadane ALgorithm

---
+++ KadaneAlgorithm.java
@@ -0,0 +1,22 @@
+public class KadaneAlgorithm{
+    //aka Maximum sum Sub Array
+    public static int maxSumSubArr(int[] nums){
+        int maxSoFar = nums[0];
+        int maxEndingHere = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+
+        if (maxSoFar < 0) {
+            return 0; // Return 0 if all elements are negative
+        }
+        return maxSoFar;
+    }
+    // public static void main(String[] args) {
+    //     int[] nums = { 2,3,-1,3,5,6,0,1};
+    //     int maxSum = maxSumSubArr(nums);
+    //     System.out.println("Maximum Sum Subarray: " + maxSum);
+    // }
+}
